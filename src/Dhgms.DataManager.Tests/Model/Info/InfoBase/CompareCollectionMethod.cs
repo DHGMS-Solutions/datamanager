73dffac518023f9bf4d888b13de93428ca71c702
David Vreony <dpvreony@users.noreply.github.com>
2016-03-22 20:48:18 +0000

    package updates

--- src/Dhgms.DataManager.Tests/Model/Info/InfoBase/CompareCollectionMethod.cs
+++ src/Dhgms.DataManager.Tests/Model/Info/InfoBase/CompareCollectionMethod.cs
@@ -20,21 +20,55 @@ namespace Dhgms.DataManager.Tests.Model.Info.InfoBase
         }
 
 
+        /// <summary>
+        /// Compares the current instance with another object of the same type.
+        /// </summary>
+        /// <param name="other">
+        /// The other.
+        /// </param>
+        /// <return>
+        /// 0 if equal, otherwise non zero
+        /// </return>
+        /// <exception cref="NotImplementedException">Method not implemented</exception>
+        /// <returns>
+        /// The compare to.
+        /// </returns>
         public override int CompareTo(CompareCollectionMethod other)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Checks if the values of two objects are the same
+        /// </summary>
+        /// <param name="other">
+        /// The other object to compare
+        /// </param>
+        /// <exception cref="NotImplementedException">Method not implemented</exception>
+        /// <returns>
+        /// True if they match, otherwise false
+        /// </returns>
         public override bool Equals(CompareCollectionMethod other)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets the hash code for the object
+        /// </summary>
+        /// <exception cref="NotImplementedException">Method not implemented</exception>
+        /// <returns>
+        /// hash code
+        /// </returns>
         public override int GetHashCode()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// The on disposing event
+        /// </summary>
+        /// <exception cref="NotImplementedException">Method not implemented</exception>
         protected override void OnDisposing()
         {
             throw new NotImplementedException();
